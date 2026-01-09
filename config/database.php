81864eb11bc8ca5371d3b4a5acd6cbce98c57b3d
mohitsolanki7051 <mohitsolanki7051@gmail.com>
2026-01-09 17:01:37 +0530

    update

--- config/database.php
+++ config/database.php
@@ -113,10 +113,22 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
         'mongodb' => [
-            'driver'   => 'mongodb',
-            'dsn'      => env('DB_URI'),
-            'database' => env('DB_DATABASE'),
-        ],
+    'driver'   => 'mongodb',
+
+    // Atlas / Production
+    'dsn'      => env('DB_URI'),
+
+    // Local fallback
+    'host'     => env('DB_HOST', '127.0.0.1'),
+    'port'     => env('DB_PORT', 27017),
+
+    'database' => env('DB_DATABASE'),
+
+    'options' => [
+        'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
+    ],
+],
+
 
 
     ],
